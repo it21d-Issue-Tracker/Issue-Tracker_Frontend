6902240ce4894d50480704010192402a5186117a
IsmaelTECE <ismael.tellez@estudiantat.upc.edu>
2025-05-24 19:09:27 +0200

    Tasca #284: CrearEditarStatuses.jsx implementado

---
+++ src/pages/CrearEditarStatuses.jsx
@@ -0,0 +1,19 @@
+// src/pages/CrearEditarStatus.jsx
+import { useParams, Link } from 'react-router-dom';
+import StatusForm from '../components/StatusForm';
+import '../css/settingsFormPage.css';
+
+export default function CrearEditarStatus() {
+  const { id } = useParams();
+  const isEdit = !!id;
+
+  return (
+    <div className="lightbox-create-edit">
+      <div style={{ textAlign: 'right' }}>
+        <Link to="/settings/status" className="back-link">×</Link>
+      </div>
+      <h2 className="title">{isEdit ? 'Edit Status' : 'New Status'}</h2>
+      <StatusForm isEdit={isEdit} />
+    </div>
+  );
+}
